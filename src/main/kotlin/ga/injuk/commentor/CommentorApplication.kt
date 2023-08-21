5e59fdf35394b13935893c707fcfd051a0d0331f
injuk <ingnoh1001@gmail.com>
2023-08-21 18:57:45 +0900

    init: 프로젝트 초기 구성

---
+++ src/main/kotlin/ga/injuk/commentor/CommentorApplication.kt
@@ -0,0 +1,11 @@
+package ga.injuk.commentor
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CommentorApplication
+
+fun main(args: Array<String>) {
+    runApplication<CommentorApplication>(*args)
+}
---
+++ src/test/kotlin/ga/injuk/commentor/CommentorApplicationTests.kt
@@ -0,0 +1,13 @@
+package ga.injuk.commentor
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CommentorApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
