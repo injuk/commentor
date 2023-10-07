6074ead441e4fe32e4a21656b9829997cc85c4a7
injuk <ingnoh1001@gmail.com>
2023-10-07 13:27:44 +0900

    feat: TransactionHelper 작업완료

--- src/main/kotlin/ga/injuk/commentor/common/TransactionHelper.kt
+++ src/main/kotlin/ga/injuk/commentor/common/TransactionHelper.kt
@@ -4,26 +4,28 @@ import org.springframework.stereotype.Component
 import org.springframework.transaction.PlatformTransactionManager
 import org.springframework.transaction.support.TransactionTemplate
 
-@Component
+/**
+ * @Component로 등록시 helper를 주입받아 execute { ... } 형태로 사용 가능
+ * 근데 commentor에서는 사용하지 않을 예정!
+ */
 class TransactionHelper(
-    private val transactionManager: PlatformTransactionManager,
+    private val txManager: PlatformTransactionManager,
 ) {
-    companion object {
-        enum class Propagation(val behavior: Int) {
-            REQUIRED(0),
-            SUPPORTS(1),
-            MANDATORY(2),
+    private val txTemplate: TransactionTemplate by lazy { TransactionTemplate(txManager) }
 
-            REQUIRES_NEW(3),
-            NOT_SUPPORTED(4),
-            NEVER(5),
-            NESTED(6),
-        }
-    }
-
-    fun <T> execute(propagation: Propagation = Propagation.REQUIRED, block: () -> T): T? {
-        return TransactionTemplate(transactionManager)
+    fun <T> execute(propagation: Propagation = Propagation.REQUIRED, block: () -> T): T?
+        = txTemplate
             .apply { propagationBehavior = propagation.behavior }
             .execute<T> { block() }
+
+    enum class Propagation(val behavior: Int) {
+        REQUIRED(0),
+        SUPPORTS(1),
+        MANDATORY(2),
+
+        REQUIRES_NEW(3),
+        NOT_SUPPORTED(4),
+        NEVER(5),
+        NESTED(6),
     }
 }
