b5f14901be1a9feaa546648eb05aca4d88e583af
injuk <ingnoh1001@gmail.com>
2023-10-30 21:05:17 +0900

    feat: object 가시성 제한자 수정

--- src/main/kotlin/ga/injuk/commentor/application/Base64Helper.kt
+++ src/main/kotlin/ga/injuk/commentor/application/Base64Helper.kt
@@ -1,8 +1,8 @@
 package ga.injuk.commentor.application
 
-import java.util.Base64
+import java.util.*
 
-object Base64Helper {
+internal object Base64Helper {
     private val encoder = Base64.getEncoder()
     private val decoder = Base64.getDecoder()
 
