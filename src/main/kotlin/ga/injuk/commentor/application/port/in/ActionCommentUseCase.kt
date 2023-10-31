60b10f86aeb5930ff9cf6674398143bcdbf34d45
injuk <ingnoh1001@gmail.com>
2023-10-31 21:06:13 +0900

    refactor: InteractionType의 위치를 usecase의 내부로 이동

--- src/main/kotlin/ga/injuk/commentor/application/port/in/ActionCommentUseCase.kt
+++ src/main/kotlin/ga/injuk/commentor/application/port/in/ActionCommentUseCase.kt
@@ -2,4 +2,16 @@ package ga.injuk.commentor.application.port.`in`
 
 import ga.injuk.commentor.application.port.dto.request.ActionCommentRequest
 
-interface ActionCommentUseCase: UseCase<ActionCommentRequest, Unit>
+interface ActionCommentUseCase : UseCase<ActionCommentRequest, Unit> {
+
+    enum class ActionType(val value: String) {
+        NEW_INTERACTION("NEW_INTERACTION"),
+        CANCEL_INTERACTION("CANCEL_INTERACTION"),
+        SWITCH_INTERACTION("SWITCH_INTERACTION");
+
+        companion object {
+            fun from(value: String?): ActionType? = ActionType.values()
+                .find { it.value == value }
+        }
+    }
+}
